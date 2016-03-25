7cdaee89bba36df2e2712826d14b0829e737f3de
somethiiing <wilsonweichengyu@gmail.com>
2016-03-24 19:51:32 -0700

    finished GET added Post, all tests pass

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -1,22 +1,62 @@
 var path = require('path');
 var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
+var Promise = require('bluebird');
 
 exports.headers = headers = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
   'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10, // Seconds.
-  // 'Content-Type': 'text/html'
+  'access-control-max-age': 10,
+  'Content-Type': 'text/html'
 };
 
-exports.serveAssets = function(res, asset, callback) {
-  // Write some code here that helps serve up your static files!
-  // (Static files are things like html (yours or archived from others...),
-  // css, or anything that doesn't change often.)
+exports.sendRedirect = function(res, loc, status) {
+  status = status || 302;
+  res.writeHead(status, {loc: loc});
+  res.end();
 };
 
+exports.sendResponse = function(res, obj, status) {
+  status = status || 200;
+  res.writeHead(status, headers);
+  res.end(obj);
+};
 
+exports.collectData = function(request, callback) {
+  var data = '';
+  request.on('data', function(chunk) {
+    data += chunk;
+  });
+  request.on('end', function() {
+    callback(data);
+  });
+};
 
+exports.send404 = function(res) {
+  exports.sendResponse(res, '404: Page not found', 404);
+};
 
-// As you progress, keep thinking about what helper functions you can put here!
+var readFile = Promise.promisify(fs.readFile);
+
+exports.serveAssets = function(res, data, callback) {
+  var encoding = {encoding: 'utf8'};
+  return readFile(archive.paths.siteAssets + data, encoding)
+    .then(function(contents) {
+      contents && exports.sendResponse(res, contents);
+    })
+    .catch(function(err) {
+      return readFile(archive.paths.archivedSites + data, encoding);
+    })
+    .then(function(contents) {
+      contents && exports.sendResponse(res, contents);
+    })
+    .catch(function(err) {
+
+      if (callback) {
+        callback();
+      } else {
+        exports.send404(res);
+      }
+    });
+};
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,64 +1,53 @@
 var path = require('path');
-var archiveHelpers = require('../helpers/archive-helpers');
-var fs = require('fs');
-var urlParser = require('url');
-var httpHelpers = require('./http-helpers.js');
-// require more modules/folders here!
+var archive = require('../helpers/archive-helpers');
+var url = require('url');
+var helpers = require('./http-helpers');
 
 var actions = {
-  'GET': function(request, response) {
-    var urlPath = url.parse(request.url).pathname;
+  'GET': function(req, res) {
+    var urlPath = url.parse(req.url).pathname;
 
-    if (urlPath === '/') {
-      urlPath = '/index.html';
-    }
-    helpers.serveAssets(response, urlPath, function() {
-      if (urlPath[0] === '/') {
-        urlPath = urlPath.slice(1);
-      }
+    if (urlPath === '/') { urlPath = '/index.html'; }
+
+    helpers.serveAssets(res, urlPath, function() {
+      if (urlPath[0] === '/') { urlPath = urlPath.slice(1); }
 
       archive.isUrlInList(urlPath, function(found) {
         if (found) {
-          helpers.sendRedirect(response, '/loading.html');
+          helpers.sendRedirect(res, '/loading.html');
         } else {
-          helpers.send404(response);
+          helpers.send404(res);
+        }
+      });
+    });
+  },
+  'POST': function(req, res) {
+    helpers.collectData(req, function(data) {
+      var url = data.split('=')[1].replace('http://', '');
+      archive.isUrlInList(url, function(found) {
+        if (found) { 
+          archive.isUrlArchived(url, function(exists) {
+            if (exists) {
+              helpers.sendRedirect(res, '/' + url);
+            } else {
+              helpers.sendRedirect(res, '/loading.html');
+            }
+          });
+        } else { 
+          archive.addUrlToList(url, function() {
+            helpers.sendRedirect(res, '/loading.html');
+          });
         }
       });
     });
   }
-  // ,
-  // 'POST': function() {
-
-  // }
-
 };
 
-exports.handleRequest = function (request, response) {
-  var url = request.url;
-  var method = request.method;
-  if (request.url === '/') {
-    fs.appendFile(archiveHelpers.paths.list, 'google' + '\n');
-
-    response.writeHead(200, httpHelpers.headers); //add callback for setHeader
-    fs.readFile(__dirname + '/public/index.html', function(err, data) {
-      if (err) {
-        // console.log('////////////////////', err);
-      } else {
-        // console.log('/////////////////////////////////', data);
-
-        response.write(data);
-        response.end();
-      }
-    });
-  } else if (request.url === '/styles.css') {
-    response.writeHead(200, httpHelpers.headers); //add callback for setHeader
-    fs.readFile(__dirname + '/public/styles.css', function(err, data) {
-      if (err) {
-        //handle error
-      } else {
-        response.write(data);
-        response.end();
-      }
-    });
+exports.handleRequest = function (req, res) {
+  var handler = actions[req.method];
+  if (handler) {
+    handler(req, res);
+  } else {
+    helpers.send404(res);
   }
 };
