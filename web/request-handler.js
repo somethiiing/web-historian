a722b1aba57d55c0885b583f653bc6d4ee2c83b0
somethiiing <wilsonweichengyu@gmail.com>
2016-03-24 19:23:40 -0700

    Perfromed much worko on 'GET'

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,35 +1,63 @@
 var path = require('path');
 var archiveHelpers = require('../helpers/archive-helpers');
 var fs = require('fs');
-var Url = require('url');
+var urlParser = require('url');
 var httpHelpers = require('./http-helpers.js');
 // require more modules/folders here!
 
-exports.handleRequest = function (req, res) {
-  var url = req.url;
-  var method = req.method;
-  if (req.url === '/') {
+var actions = {
+  'GET': function(request, response) {
+    var urlPath = url.parse(request.url).pathname;
+
+    if (urlPath === '/') {
+      urlPath = '/index.html';
+    }
+    helpers.serveAssets(response, urlPath, function() {
+      if (urlPath[0] === '/') {
+        urlPath = urlPath.slice(1);
+      }
+
+      archive.isUrlInList(urlPath, function(found) {
+        if (found) {
+          helpers.sendRedirect(response, '/loading.html');
+        } else {
+          helpers.send404(response);
+        }
+      });
+    });
+  }
+  // ,
+  // 'POST': function() {
+
+  // }
+
+};
+
+exports.handleRequest = function (request, response) {
+  var url = request.url;
+  var method = request.method;
+  if (request.url === '/') {
     fs.appendFile(archiveHelpers.paths.list, 'google' + '\n');
 
-    res.writeHead(200, httpHelpers.headers); //add callback for setHeader
+    response.writeHead(200, httpHelpers.headers); //add callback for setHeader
     fs.readFile(__dirname + '/public/index.html', function(err, data) {
       if (err) {
         // console.log('////////////////////', err);
       } else {
         // console.log('/////////////////////////////////', data);
 
-        res.write(data);
-        res.end();
+        response.write(data);
+        response.end();
       }
     });
-  } else if (req.url === '/styles.css') {
-    res.writeHead(200, httpHelpers.headers); //add callback for setHeader
+  } else if (request.url === '/styles.css') {
+    response.writeHead(200, httpHelpers.headers); //add callback for setHeader
     fs.readFile(__dirname + '/public/styles.css', function(err, data) {
       if (err) {
         //handle error
       } else {
-        res.write(data);
-        res.end();
+        response.write(data);
+        response.end();
       }
     });
   }
